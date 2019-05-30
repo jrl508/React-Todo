d84527cb2ffb8a06bdc9cf9d2ee3830197dee356
jrl508 <jrl47@yahoo.com>
2019-05-30 19:04:58 -0400

    added strike through styling for completed task

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './Todo.css'
 
 function Todo(props){
     return(
         <div 
+        className={`todo ${props.task.completed ? 'complete' : ''}`}
         onClick={()=> props.crossTask(props.task.id)}
         >
             <p>
